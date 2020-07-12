0a3e4663706185776389743f9c616c7b3df5d8b8
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-07-13 03:28:14 +0530

    Improved battery block

--- blocks/battery.c
+++ blocks/battery.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "../util.h"
 #include "battery.h"
+
+#define UDEVNOTRELIABLE
 
 #define ICON0				COL1 "" COL0
 #define ICON1				COL1 "" COL0
@@ -12,8 +15,14 @@
 #define ICON4				COL1 "" COL0
 #define ICON5				COL1 "" COL0
 
+#define BATM				5   /* hibernate below this level */
+#define BATC				10  /* critical level */
+#define BATL				20  /* low level */
+#define BATP				40  /* warn to plug in charger below this level */
+#define BATU				80  /* warn to unplug charger below this level */
+
 #define BATCAPFILE			"/sys/class/power_supply/BAT0/capacity"
-#define	ACSTATEFILE			"/sys/class/power_supply/AC/online"
+#define	BATSTATEFILE			"/sys/class/power_supply/BAT0/status"
 #define BATRATEFILE			"/sys/class/power_supply/BAT0/current_now"
 #define BATCNOWFILE			"/sys/class/power_supply/BAT0/charge_now"
 #define BATCFULLFILE			"/sys/class/power_supply/BAT0/charge_full"
@@ -24,75 +33,128 @@
 	uspawn((char *[]) { "/usr/bin/dunstify", "-r", "2120", "-t", t, "-u", "critical", "BatMon", msg, NULL })
 #define CNNOTIFY(t, msg) \
 	cspawn((char *[]) { "/usr/bin/dunstify", "-r", "2120", "-t", t, "BatMon", msg, NULL })
+#define CCNOTIFY(t, msg) \
+	cspawn((char *[]) { "/usr/bin/dunstify", "-r", "2120", "-t", t, "-u", "critical", "BatMon", msg, NULL })
 
-enum { Normal, Critical, Low, Plug, Unplug };
+enum { Normal, Critical, Low, Plug, Unplug }; /* level */
+enum { Unknown, Discharging, Charging, Full, NotCharging }; /* state */
 
-static int state;
+int
+chargingstatus()
+{
+        int c;
+        FILE *fp;
+
+        if (!(fp = fopen(BATSTATEFILE, "r")))
+                return Unknown;
+        if ((c = fgetc(fp)) == EOF) {
+                fclose(fp);
+                return Unknown;
+        }
+        fclose(fp);
+        if (c == 'D') /* Discharging */
+                return Discharging;
+        else if (c == 'C') /* Charging */
+                return Charging;
+        else if (c == 'F') /* Full */
+                return Full;
+        else if (c == 'U') /* Unknown */
+                return Unknown;
+        else /* Not Charging */
+                return NotCharging;
+}
 
 void
 batteryu(char *str, int ac)
 {
+#ifdef UDEVNOTRELIABLE
+        static int charging = -1;
+#endif
+        static int level = Normal;
         int bat;
 
         if (!readint(BATCAPFILE, &bat)) {
-                *str = '\0';
+                strcpy(str, ICON0);
                 return;
         }
         if (ac == NILL) {
-                if (!readint(ACSTATEFILE, &ac)) {
-                        *str = '\0';
-                        return;
+                switch (chargingstatus()) {
+                        case Charging:
+                        case Full:
+#ifdef UDEVNOTRELIABLE
+                                if (charging != 1) {
+                                        if (charging != -1 && bat < BATU)
+                                                UNNOTIFY("1000", "Charger plugged in");
+                                        charging = 1;
+                                }
+#endif
+                                goto onac;
+                        default:
+#ifdef UDEVNOTRELIABLE
+                                if (charging != 0) {
+                                        if (charging != -1 && bat > BATP)
+                                                UNNOTIFY("1000", "Charger plugged out");
+                                        charging = 0;
+                                }
+#endif
+                                goto onbat;
                 }
         } else {
                 if (ac) {
-                        UNNOTIFY("1000", "Charger plugged in");
-                        goto yac;
+                        if (bat < BATU)
+                                UNNOTIFY("1000", "Charger plugged in");
+#ifdef UDEVNOTRELIABLE
+                        charging = 1;
+#endif
+                        goto onac;
                 } else {
-                        UNNOTIFY("1000", "Charger plugged out");
-                        goto nac;
+                        if (bat > BATP)
+                                UNNOTIFY("1000", "Charger plugged out");
+#ifdef UDEVNOTRELIABLE
+                        charging = 0;
+#endif
+                        goto onbat;
                 }
         }
-        if (ac) {
-yac:
-                snprintf(str, CMDLENGTH, ICON5 "%d%%", bat);
-                if (bat >= 80) { 
-                        if (state != Unplug) {
-                                state = Unplug;
-                                UNNOTIFY("0", "Unplug the charger");
-                        }
-                } else if (state != Normal)
-                        state = Normal;
+onac:
+        snprintf(str, CMDLENGTH, ICON5 "%d%%", bat);
+        if (bat >= BATU) {
+                if (level != Unplug) {
+                        UNNOTIFY("0", "Unplug the charger");
+                        level = Unplug;
+                }
+        } else if (level != Normal)
+                level = Normal;
+        return;
+onbat:
+        if (bat >= BATU) {
+                snprintf(str, CMDLENGTH, ICON4 "%d%%", bat);
+                if (level != Normal)
+                        level = Normal;
+        } else if (bat > BATP) {
+                snprintf(str, CMDLENGTH, ICON3 "%d%%", bat);
+                if (level != Normal)
+                        level = Normal;
+        } else if (bat > BATL) {
+                snprintf(str, CMDLENGTH, ICON2 "%d%%", bat);
+                if (level != Plug) {
+                        UNNOTIFY("0", "Plug in the charger");
+                        level = Plug;
+                }
         } else {
-nac:
-                if (bat >= 80) {
-                        snprintf(str, CMDLENGTH, ICON4 "%d%%", bat);
-                        if (state != Normal)
-                                state = Normal;
-                } else if (bat > 40) {
-                        snprintf(str, CMDLENGTH, ICON3 "%d%%", bat);
-                        if (state != Normal)
-                                state = Normal;
-                } else if (bat > 20) {
-                        snprintf(str, CMDLENGTH, ICON2 "%d%%", bat);
-                        if (state != Plug) {
-                                state = Plug;
-                                UNNOTIFY("0", "Plug in the charger");
+                snprintf(str, CMDLENGTH, ICON1 "%d%%", bat);
+                if (bat > BATC) {
+                        if (level != Low) {
+                                UNNOTIFY("0", "Battery level is low!");
+                                level = Low;
                         }
                 } else {
-                        snprintf(str, CMDLENGTH, ICON1 "%d%%", bat);
-                        if (bat > 10) {
-                                if (state != Low) {
-                                        state = Low;
-                                        UNNOTIFY("0", "Battery level is low!");
-                                }
-                        } else {
-                                if (state != Critical) {
-                                        state = Critical;
-                                        UCNOTIFY("0", "Battery level is critical!");
-                                }
-                                if (bat <= 5)
-                                        uspawn((char *[]) { "/usr/bin/systemctl", "hibernate", NULL });
+                        if (level != Critical) {
+                                UCNOTIFY("0", "Battery level is critical!");
+                                level = Critical;
                         }
+                        if (bat <= BATM)
+                                uspawn((char *[]) { "/usr/bin/systemctl", "hibernate", NULL });
                 }
         }
 }
@@ -101,23 +163,45 @@ void
 batteryc(int button)
 {
         int cur, rate;
-        int ac, cnow, cfull;
         int hr, mn;
 
-        if (!readint(BATRATEFILE, &rate))
-                return;
-        if (!rate)
-                CNNOTIFY("2000", "Battery fully charged");
-        if (!readint(BATCNOWFILE, &cnow))
-                return;
-        if (!readint(ACSTATEFILE, &ac))
-                return;
-        if (ac) {
-                if (!readint(BATCFULLFILE, &cfull))
+        switch (chargingstatus()) {
+                case Unknown:
+                        CCNOTIFY("0", "Battery status is unknown!");
                         return;
-                cur = cfull - cnow;
-        } else
-                cur = cnow;
+                case Full:
+                        CNNOTIFY("2000", "Battery fully charged");
+                        return;
+                case Discharging:
+                case NotCharging:
+                        if (!readint(BATCNOWFILE, &cur)) {
+                                CCNOTIFY("0", "Couldn't read" BATCNOWFILE);
+                                return;
+                        }
+                        break;
+                case Charging:
+                {
+                        int cnow;
+
+                        if (!readint(BATCFULLFILE, &cur)) {
+                                CCNOTIFY("0", "Couldn't read" BATCFULLFILE);
+                                return;
+                        }
+                        if (!readint(BATCNOWFILE, &cnow)) {
+                                CCNOTIFY("0", "Couldn't read" BATCNOWFILE);
+                                return;
+                        }
+                        cur -= cnow;
+                }
+        }
+        if (!readint(BATRATEFILE, &rate)) {
+                CCNOTIFY("0", "Couldn't read" BATRATEFILE);
+                return;
+        }
+        if (!rate) {
+                CCNOTIFY("0", "Unexpected current value!");
+                return;
+        }
         hr = cur / rate;
         mn = ((cur * 60) / rate) - hr * 60;
         switch (hr) {
