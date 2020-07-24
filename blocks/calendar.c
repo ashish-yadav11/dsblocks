ce7d4c32b7a6dac40e1d95c2b3101961307d6f7f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-07-24 21:36:37 +0530

    Improved calendar block

--- blocks/calendar.c
+++ blocks/calendar.c
@@ -9,34 +9,14 @@
 void
 calendaru(char *str, int sigval)
 {
-        char *day, *month;
+        static char *days[] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+        static char *months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+                                  "Aug", "Sep", "Oct", "Nov", "Dec" };
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
 
-        switch (tm.tm_wday) {
-                case 0:  day   = "Sun"; break;
-                case 1:  day   = "Mon"; break;
-                case 2:  day   = "Tue"; break;
-                case 3:  day   = "Wed"; break;
-                case 4:  day   = "Thu"; break;
-                case 5:  day   = "Fri"; break;
-                default: day   = "Sat"; break;
-        }
-        switch (tm.tm_mon) {
-                case 0:  month = "Jan"; break;
-                case 1:  month = "Feb"; break;
-                case 2:  month = "Mar"; break;
-                case 3:  month = "Apr"; break;
-                case 4:  month = "May"; break;
-                case 5:  month = "Jun"; break;
-                case 6:  month = "Jul"; break;
-                case 7:  month = "Aug"; break;
-                case 8:  month = "Sep"; break;
-                case 9:  month = "Oct"; break;
-                case 10: month = "Nov"; break;
-                default: month = "Dec"; break;
-        }
-        snprintf(str, CMDLENGTH, ICON "%s, %02d %s", day, tm.tm_mday, month);
+        snprintf(str, CMDLENGTH, ICON "%s, %02d %s",
+                 days[tm.tm_wday], tm.tm_mday, months[tm.tm_mon]);
 }
 
 void
@@ -47,11 +27,11 @@ calendarc(int button)
                 {
                 }
                         break;
-                case 2:
+                case 3:
                 {
                 }
                         break;
-                case 3:
+                case 2:
                 {
                 }
                         break;
