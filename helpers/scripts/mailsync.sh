40e7e4f5b8fc897225834e3174c92739baa12bc4
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-05 11:24:13 +0530

    Improved mailsync.sh

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -2,9 +2,8 @@
 exec >/dev/null 2>&1
 sigdsblocks 2 1
 if ping -c1 imap.gmail.com ; then
-    while PID=$(pidof -s /usr/bin/mbsync) ; do
-        tail --pid="$PID" -f /dev/null
-    done
+    PID=$(pidof -s /usr/bin/mbsync) && tail --pid="$PID" -f /dev/null
+    pidof -s /usr/bin/mbsync && exit
     sigdsblocks 2 -2
     if mbsync iiser ; then
         sigdsblocks 2 3
