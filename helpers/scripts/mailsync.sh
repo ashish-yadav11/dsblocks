e6fce9b9675831c2520d36783b3cd74c561f4917
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-08 12:08:25 +0530

    Improved mailsync.sh

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -1,9 +1,29 @@
 #!/bin/dash
 exec >/dev/null 2>&1
+
+# to make sure no more than 2 instances can be active at the same time
+if read -r numactive </tmp/mailsync.num ; then
+    case "$numactive" in
+        0)
+            echo 1 >/tmp/mailsync.num
+            remove=1
+            ;;
+        1)
+            echo 2 >/tmp/mailsync.num
+            remove=0
+            ;;
+        *)
+            exit ;;
+    esac
+else
+    echo 1 >/tmp/mailsync.num
+    remove=1
+fi
+
+# main body
 sigdsblocks 2 1
 if ping -c1 imap.gmail.com ; then
     PID=$(pidof -s /usr/bin/mbsync) && tail --pid="$PID" -f /dev/null
-    pidof -s /usr/bin/mbsync && exit
     sigdsblocks 2 -2
     if mbsync iiser ; then
         sigdsblocks 2 3
@@ -14,3 +34,13 @@ if ping -c1 imap.gmail.com ; then
 else
     sigdsblocks 2 5
 fi
+
+# cleanup
+case "$remove" in
+    0)
+        echo 1 >/tmp/mailsync.num
+        ;;
+    1)
+        rm -f /tmp/mailsync.num
+        ;;
+esac
