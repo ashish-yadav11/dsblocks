270c4e9f2214003ad9536f95cfbc8819bcc90e2d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-08 13:04:04 +0530

    Fixed a bug in mailsync.sh

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -1,18 +1,22 @@
 #!/bin/dash
 exec >/dev/null 2>&1
 
-read -r lock </tmp/mailsync.lock
-[ -n "$lock" ] && exit
+# allow only two instances of the script at one time
+if read -r lock </tmp/mailsync.lock ; then
+    case "$lock" in
+        0) echo 1 >/tmp/mailsync.lock ;;
+        1) exit ;;
+    esac
+else
+    echo 0 >/tmp/mailsync.lock
+    remove=1
+fi
 
+# main body
 sigdsblocks 2 1
 if ping -c1 imap.gmail.com ; then
-    if PID=$(pidof -s /usr/bin/mbsync) ; then
-        echo 1 >/tmp/mailsync.lock
-        remove=1
-        tail --pid="$PID" -f /dev/null
-    fi
+    PID=$(pidof -s /usr/bin/mbsync) && tail --pid="$PID" -f /dev/null
     sigdsblocks 2 -2
-    [ -n "$remove" ] && rm -f /tmp/mailsync.lock
     if mbsync iiser ; then
         sigdsblocks 2 3
     else
@@ -21,4 +25,11 @@ if ping -c1 imap.gmail.com ; then
     notmuch new
 else
     sigdsblocks 2 5
+fi
+
+# cleanup
+if [ -n "$remove" ] ; then
+    rm -f /tmp/mailsync.lock
+else
+    echo 0 >/tmp/mailsync.lock
 fi
