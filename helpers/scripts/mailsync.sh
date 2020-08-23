f6e97e82a72ea7df5d7db1078d625337f3b0549f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-24 04:01:52 +0530

    Changed waiting period from 300 to 150 in mailsync.sh

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -4,7 +4,7 @@ exec >/dev/null 2>&1 8>/tmp/mailsync.1.lock
 if ! flock -n 8 ; then
     exec 9>/tmp/mailsync.2.lock
     flock -n 9 || exit
-    flock -w 300 8 || exit
+    flock -w 150 8 || exit
 fi
 
 sigdsblocks 2 1
