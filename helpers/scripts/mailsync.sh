081766fe21d7a662b9ce89f7f91f4ccecfcfe3cd
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-24 03:34:42 +0530

    Cleaned up mailsync.sh

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -1,18 +1,10 @@
 #!/bin/dash
-exec >/dev/null 2>&1
-
-lockfile1=/tmp/mailsync.1.lock
-lockfile2=/tmp/mailsync.2.lock
-
-exec 8>"$lockfile1" 9>"$lockfile2"
+exec >/dev/null 2>&1 8>/tmp/mailsync.1.lock
 
 if ! flock -n 8 ; then
+    exec 9>/tmp/mailsync.2.lock
     flock -n 9 || exit
-#    trap 'rm -rf "$lockfile2"; exit' EXIT HUP INT TERM
     flock -w 300 8 || exit
-#    trap 'rm -rf "$lockfile1" "$lockfile2"; exit' EXIT HUP INT TERM
-#else
-#    trap 'rm -rf "$lockfile1"; exit' EXIT HUP INT TERM
 fi
 
 sigdsblocks 2 1
