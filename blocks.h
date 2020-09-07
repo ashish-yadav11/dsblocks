7cb638b8c3fc34a4121b8980ed73e48d7c829f09
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-09-08 00:42:46 +0530

    Mentioning realtime is not really required

--- blocks.h
+++ blocks.h
@@ -12,7 +12,7 @@
  * If interval is set to a negative value, the block will never be updated in the main loop.
  * Set signal to 0 if signalling is not required for the block.
  * Signal must be less than 10 for clickable blocks.
- * If multiple realtime signals are pending, then the lowest numbered signal is delivered first. */
+ * If multiple signals are pending, then the lowest numbered one will be delivered first. */
 
 /* funcu - function responsible for updating what is shown on the status
  * funcc - function responsible for handling clicks on the block */
