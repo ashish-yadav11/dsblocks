a5cb0114b86101198121a4b3d87956c128123486
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-10-05 23:46:23 +0530

    Removed a bug involving trap in hotspot.sh

--- helpers/scripts/hotspot.sh
+++ helpers/scripts/hotspot.sh
@@ -2,9 +2,10 @@
 case "$1" in
     initiate)
         iwi=$(nmcli -t -f DEVICE connection show --active | head -n1)
-        trap 'sigdsblocks 3 0; exit' EXIT HUP INT TERM
+        trap 'sigdsblocks 3 0; exit' HUP INT TERM
         sigdsblocks 3 1
         create_ap wlp5s0 "${iwi:-eno1}" hp 98761234
+        sigdsblocks 3 0
         ;;
     terminate)
         create_ap --stop wlp5s0
