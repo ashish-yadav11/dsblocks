44bcab9280bf2fe9b20560fee9445661002a00a7
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-11-22 13:34:50 +0530

    Improved helpers

--- helpers/nm_dispatcher/01-dsblocks.sh
+++ helpers/nm_dispatcher/01-dsblocks.sh
@@ -1,5 +1,8 @@
 #!/bin/dash
+
+# This script is only for NetworkManager users
+# It should be placed in /etc/NetworkManager/dispatcher.d/
+
 case $2 in
-    up|down)
-        /home/ashish/.local/bin/sigdsblocks 3
+    up|down) /home/ashish/.local/bin/sigdsblocks 3 ;;
 esac
--- helpers/sleep_hook/update_status.sh
+++ helpers/sleep_hook/update_status.sh
@@ -1,8 +1,15 @@
 #!/bin/dash
+
+# This script is only for systemd-logind users
+# It should be placed in /usr/lib/systemd/system-sleep/
+
 case $1 in
     post)
-        /home/ashish/.local/bin/sigdsblocks 1 # calendar
-        /home/ashish/.local/bin/sigdsblocks 4 # battery
-        /home/ashish/.local/bin/sigdsblocks 6 # cpu temperature
+        sigdsblocks=/home/ashish/.local/bin/sigdsblocks
+
+        $sigdsblocks 1 # calendar
+        $sigdsblocks 2 # volume
+        $sigdsblocks 4 # battery
+        $sigdsblocks 6 # cpu temperature
         ;;
 esac
