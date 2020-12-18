c5916a6e2744264a2cb5e3fb9a756226138f790f
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-18 21:51:02 +0530

    Improved hotspot_launch.sh

--- helpers/scripts/hotspot_launch.sh
+++ helpers/scripts/hotspot_launch.sh
@@ -5,7 +5,7 @@ notify="notify-send -h string:x-canonical-private-synchronous:hotspot"
 if [ -n "$(create_ap --list-running)" ] ; then
     sudo $hotspot terminate
 else
-    if nmcli -t -f DEVICE,STATE device status | grep -qE 'wlp5s0:(connected)|(connecting)' ; then
+    if nmcli -t -f DEVICE,STATE device status | grep -qEm1 'wlp5s0:(connected)|(connecting)' ; then
         $notify -t 3000 Hotspot "Your adapter can not be a station and an AP at the same time"
         exit
     fi
