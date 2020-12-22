3badea3c095404a1a835d1b3452a66a4501aa42a
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-22 19:54:26 +0530

    Replace raw characters with \xXX

--- util.h
+++ util.h
@@ -1,11 +1,11 @@
 #include "shared.h"
 
-#define COL0                            "" /* default status color */
-#define COL1                            "" /* default icon color */
-#define COL2                            "\r" /* alternate icon color */
-#define COL3                            "" /* mail block - frozen */
-#define COL4                            "" /* mail block - MAILSYNC started */
-#define COL5                            "" /* mail block - syncing */
+#define COL0                            "\x0b" /* default status color */
+#define COL1                            "\x0c" /* default icon color */
+#define COL2                            "\x0d" /* alternate icon color */
+#define COL3                            "\x0e" /* mail block - frozen */
+#define COL4                            "\x0f" /* mail block - MAILSYNC started */
+#define COL5                            "\x10" /* mail block - syncing */
 
 #define LENGTH(X)                       (sizeof X / sizeof X[0])
 #define SCRIPT(name)                    "/home/ashish/.scripts/"name
