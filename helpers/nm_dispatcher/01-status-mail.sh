849cb0209770567782bcdb14862fbed41333862e
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-24 18:45:07 +0530

    Improved comments in nm_dispatcher

--- helpers/nm_dispatcher/01-status-mail.sh
+++ helpers/nm_dispatcher/01-status-mail.sh
@@ -2,6 +2,7 @@
 
 # This script is only for NetworkManager users
 # It should be placed in /etc/NetworkManager/dispatcher.d/
+# NetworkManager-dispatcher.service should be active
 
 case $2 in
     up|down) sigdsblocks 3 ;;
