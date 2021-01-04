9e735abf929901138d7eaf6feeed17c9f6a8e45d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-04 18:15:49 +0530

    Improved hotspot scripts

--- helpers/scripts/hotspot.sh
+++ helpers/scripts/hotspot.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
+wifi_interface=wlp5s0
+
 case $1 in
     initiate)
-        iwi=$(nmcli -t -f DEVICE connection show --active | head -n1)
-        trap 'sigdsblocks 4 0; exit' HUP INT TERM
+        fallback_interface=eno1
+
+        nmcli device disconnect "$wifi_interface"
+        active_interface=$(nmcli -t -f DEVICE connection show --active)
+        newline='
+'
+        active_interface=${active_interface%%$newline*}
+        trap 'sigdsblocks 4 0; exit 0' HUP INT TERM
         sigdsblocks 4 1
-        create_ap wlp5s0 "${iwi:-eno1}" hp 98761234; status=$?
+        create_ap "$wifi_interface" "${active_interface:-$fallback_interface}" pc 11111111
+        status=$?
         sigdsblocks 4 0
         exit "$status"
         ;;
     terminate)
-        create_ap --stop wlp5s0
+        create_ap --stop "$wifi_interface"
         ;;
     fix-unmanaged)
         [ -z "$(create_ap --list-running)" ] && create_ap --fix-unmanaged
