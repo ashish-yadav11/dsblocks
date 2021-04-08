6f326a61d9c3778406fd4d78f81ed5096567fb97
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-04-08 14:46:34 +0530

    Added IFS='' to while read - pulse_daemon.sh

--- helpers/daemons/pulse_daemon.sh
+++ helpers/daemons/pulse_daemon.sh
@@ -2,7 +2,7 @@
 # This script should be autostarted on startup
 # It signals volume block to update on audio related events
 pactl subscribe |
-    while read -r event ; do
+    while IFS='' read -r event ; do
         case $event in
             *" sink "*) sigdsblocks 2 ;;
         esac
