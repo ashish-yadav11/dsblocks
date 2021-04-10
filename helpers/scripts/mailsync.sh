a5e262b46d545bdf04dd241563f9e349a9e8e46b
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-04-10 13:35:47 +0530

    mbsync sometimes freezes

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -7,12 +7,17 @@ mbsync_channel=-a
 # syncing and one waiting for the other to finish syncing.
 
 sigdsblocks 3 -1
-exec >/dev/null 2>&1 8>/tmp/mailsync.1.lock 9>/tmp/mailsync.2.lock
+exec >/dev/null 2>&1 8<>/tmp/mailsync.1.lock 9<>/tmp/mailsync.2.lock
 if ! flock -n 8 ; then
     flock -n 9 || exit
-    flock 8
+    if ! flock -w120 8 ; then
+        read -r PID <&8 && pkill mbsync -P "$PID"
+        flock -w15 8 || exit
+    fi
 fi
+echo "$$" >&8
 mbsync "$mbsync_channel"
+: >/tmp/mailsync.1.lock
 sigval=$?
 notmuch new
 flock -n 9 && sigdsblocks 3 "$sigval"
