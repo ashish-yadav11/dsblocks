e16bf86ad61c0157993955c21f7b673a511c52ca
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-06-21 13:43:08 +0530

    /tmp is volatile - mailsync.sh

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -3,11 +3,14 @@
 # signals mail block whether the sync was successful or not
 mbsync_channel=-a
 
+lockfile1=$XDG_RUNTIME_DIR/mailsync.1.lock
+lockfile2=$XDG_RUNTIME_DIR/mailsync.2.lock
+
 # At max two instances of the script are allowed to run simultaneously, one
 # syncing and one waiting for the other to finish syncing.
 
 sigdsblocks 3 -1
-exec >/dev/null 2>&1 8<>/tmp/mailsync.1.lock 9<>/tmp/mailsync.2.lock
+exec >/dev/null 2>&1 8<>"$lockfile1" 9<>"$lockfile2"
 if ! flock -n 8 ; then
     flock -n 9 || exit
     flock 8
