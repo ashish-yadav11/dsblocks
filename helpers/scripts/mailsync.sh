6aad83d7f13df754d97eae13f23aa9d6d0b06540
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-06-21 18:41:37 +0530

    Runtime dir is a better option - mailsync.sh

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -3,8 +3,8 @@
 # signals mail block whether the sync was successful or not
 mbsync_channel=-a
 
-lockfile1=/var/local/dsblocks/mailsync.1.lock
-lockfile2=/var/local/dsblocks/mailsync.2.lock
+lockfile1=$XDG_RUNTIME_DIR/mailsync.1.lock
+lockfile2=$XDG_RUNTIME_DIR/mailsync.2.lock
 
 # At max two instances of the script are allowed to run simultaneously, one
 # syncing and one waiting for the other to finish syncing.
