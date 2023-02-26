3b27e9eae87bf17d8bbab6f95f66b2968b2d6e73
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2023-02-26 16:26:55 +0530

    Added workaround for mailsync script

--- helpers/scripts/mailsync.sh
+++ helpers/scripts/mailsync.sh
@@ -2,6 +2,7 @@
 # This script syncs and indexes mail database using mbsync and notmuch and
 # signals mail block whether the sync was successful or not
 mbsync_channel=-a
+timeout=300
 
 lockfile1="$XDG_RUNTIME_DIR/mailsync.1.lock"
 lockfile2="$XDG_RUNTIME_DIR/mailsync.2.lock"
@@ -16,7 +17,8 @@ if ! flock -n 8 ; then
     flock -n 9 || exit
     flock 8
 fi
-mbsync "$mbsync_channel"
+# Workaround for freezing issue of mbsync.
+timeout "$timeout" mbsync "$mbsync_channel"
 sigval="$?"
 notmuch new
 flock -n 9 && sigdsblocks 3 "$sigval"
